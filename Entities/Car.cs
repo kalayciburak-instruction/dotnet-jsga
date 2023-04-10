1deea85974c0ae058c4e79b35ef4502e1c0c8a15
Burak <kalayciburak1996@gmail.com>
2023-04-10 15:53:04 +0300

    04-10-23

---
+++ Entities/Car.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Car : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public int ModelYear { get; set; }
+        public double DailyPrice { get; set; }
+        public string Description { get; set; }
+        public Brand Brand { get; set; }
+        public Color Color { get; set; }
+
+    }
+}
---
+++ Entities/Color.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Color : IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
